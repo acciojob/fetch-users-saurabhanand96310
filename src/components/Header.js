862f9539d5e1ffdf6d65167b6f61cf77ec3e3126
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-12-27 19:51:29 +0000

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,74 +1,73 @@
 import Axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const Header=()=>{
-      const [Data ,setData]=useState([]);
-      const [loading, setLoading]=useState(false);
+const Header = () => {
+  const [Data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const handleBTN=()=>{
-        setLoading(true)
-            Axios({
-                url:"https://reqres.in/api/users",
-                method:"GET"
-            }).then(Response => {
-                console.log(Response.data)
-                setData(Response.data.data)
-                setLoading(false)
-            }).catch(err=>{
-                console.log(err)
-                setLoading(false)
-            }
-            )
-        
-    }
-return(
+  const handleBTN = () => {
+    setLoading(true);
+    Axios({
+      url: "https://reqres.in/api/users",
+      method: "GET",
+    })
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  return (
     <div className="header">
-        <button className="btn" onClick={handleBTN}>Get user List</button>
-        {/* <div className="header-span">
-            <span>Full Name</span>
-            <span>Last Name</span>
-            <span>Email</span>
-            <span>Avatar</span>
-        </div> */}
-    <table>
-    <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Avatar</th>
-        </tr>
-      </thead>
-    </table>
-        <div>
-        <table>
-  {loading ? (
-    <caption>Loading...</caption> // Use caption for loading message in tables
-  ) : Data.length > 0 ? (
-    <>
+      <button className="btn" onClick={handleBTN}>
+        Get User List
+      </button>
       
-      <tbody>
-        {Data.map((item) => (
-          <tr key={item.id}>
-            <td>{item.first_name}</td>
-            <td>{item.last_name}</td>
-            <td>{item.email}</td>
-            <td>
-              <img src={item.avatar} alt={`${item.first_name} avatar`} height="50" width="50" />
-            </td>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
           </tr>
-        ))}
-      </tbody>
-    </>
-  ) : (
-    <caption>No data found to display</caption> // Use caption for empty state
-  )}
-</table>
-
-        </div>
+        </thead>
+        
+        {/* Display loading message or data */}
+        <tbody>
+          {loading ? (
+            <tr>
+              <td colSpan="4" style={{ textAlign: "center" }}>
+                Loading...
+              </td>
+            </tr>
+          ) : Data.length > 0 ? (
+            Data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>
+                  <img src={item.avatar} alt={`${item.first_name} avatar`} height="50" width="50" />
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4" style={{ textAlign: "center" }}>
+                No data found to display
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
-)
+  );
 };
-
 
 export default Header;
