96f71c6f9d6b411b916335d45e9f03ccf0372629
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-12-27 17:55:15 +0000

    table created

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,23 +24,47 @@ const Header=()=>{
 return(
     <div className="header">
         <button className="btn" onClick={handleBTN}>Get user List</button>
-        <div className="header-span">
+        {/* <div className="header-span">
             <span>Full Name</span>
             <span>Last Name</span>
             <span>Email</span>
             <span>Avatar</span>
-        </div>
+        </div> */}
+    <table>
+    <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Avatar</th>
+        </tr>
+      </thead>
+    </table>
         <div>
-            <table>
-         {loading?<p>Loading...</p>:Data.length>0?Data.map((item) => (
-                <ul key={item.id} className="ul-item"> 
-                  <li>{item.first_name}</li> 
-                  <li>{item.last_name}</li>
-                  <li>{item.email}</li>
-                  <li><img src={item.avatar} alt={`${item.first_name} avatar`} /></li>
-                </ul>
-              )):<p>No data found'</p>} 
-              </table>
+        <table>
+  {loading ? (
+    <caption>Loading...</caption> // Use caption for loading message in tables
+  ) : Data.length > 0 ? (
+    <>
+      
+      <tbody>
+        {Data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.first_name}</td>
+            <td>{item.last_name}</td>
+            <td>{item.email}</td>
+            <td>
+              <img src={item.avatar} alt={`${item.first_name} avatar`} height="50" width="50" />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </>
+  ) : (
+    <caption>No data found to display</caption> // Use caption for empty state
+  )}
+</table>
+
         </div>
     </div>
 )
